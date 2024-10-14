53ecc187e1599bd7abc4bc5f595e6fcc2e6735b8
jafarbekyusupov <github.jafarbekyusupov@gmail.com>
2024-10-14 21:30:10 +0500

    Create 231A_Team.cpp

---
+++ cpp/231A_Team.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, cnt = 0;
+    cin >> n;
+    for(int i=0; i<n; i++){
+        int a, b, c;
+        cin >> a >> b >> c;
+        if (a + b + c > 1){ cnt++;  }
+    }
+    cout << cnt << endl;
+    return 0;
+}
