83b5b32b6faec1b4f76c95a89e179edd7afc215d
jafarbekyusupov <github.jafarbekyusupov@gmail.com>
2024-10-15 00:44:04 +0500

    Update 112A_Petya-and-Strings.cpp

--- cpp/112A_Petya-and-Strings.cpp
+++ cpp/112A_Petya-and-Strings.cpp
@@ -1,1 +1,14 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
 
+int main() {
+    std::string string1, string2;
+    std::cin >> string1 >> string2;
+    std::transform(string1.begin(), string1.end(), string1.begin(), ::tolower);
+    std::transform(string2.begin(), string2.end(), string2.begin(), ::tolower);
+    if (string1 < string2){ std::cout << "-1" << std::endl;} 
+    else if (string1 > string2){    std::cout << "1" << std::endl;} 
+    else {  std::cout << "0" << std::endl;}
+    return 0;
+}
