7409d4af82a731ac2c0243423aef1f6b792d4f51
jafarbekyusupov <github.jafarbekyusupov@gmail.com>
2024-10-15 00:49:39 +0500

    Update 112A_Petya-and-Strings.cpp

--- cpp/112A_Petya-and-Strings.cpp
+++ cpp/112A_Petya-and-Strings.cpp
@@ -2,7 +2,7 @@
 #include <string>
 #include <algorithm>
 
-int main() {
+void using_if_else(){
     std::string string1, string2;
     std::cin >> string1 >> string2;
     std::transform(string1.begin(), string1.end(), string1.begin(), ::tolower);
@@ -10,5 +10,19 @@ int main() {
     if (string1 < string2){ std::cout << "-1" << std::endl;} 
     else if (string1 > string2){    std::cout << "1" << std::endl;} 
     else {  std::cout << "0" << std::endl;}
+}
+
+void using_ternary(){
+    std::string str1, str2;
+    std::cin >> str1 >> str2;
+    std::transform(str1.begin(), str1.end(), str1.begin(), ::tolower);
+    std::transform(str2.begin(), str2.end(), str2.begin(), ::tolower);
+    std::cout << (str1 < str2 ? "-1" : (str1 > str2 ? "1" : "0")) << std::endl;
+}
+
+
+int main() {
+    // using_ternary();
+    // using_if_else();
     return 0;
 }
