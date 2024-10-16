7e27b3a126e544152e3f7d3dc90fe4c918070880
Ja'farbek Yusupov <github.jafarbekyusupov@gmail.com>
2024-10-16 22:37:05 +0500

    Create 281A_Word-Capitalization.cpp

---
+++ cpp/281A_Word-Capitalization.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string word;
+    cin >> word;
+    word[0] = word[0] >= 'a' && word[0] <= 'z' ? (word[0] - 'a' + 'A') : word[0];
+    cout << word << endl;
+    return 0;
+}
