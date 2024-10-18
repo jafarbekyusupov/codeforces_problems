25715c7a4f4f385cd24d75529ed692582d9bd771
Ja'farbek Yusupov <github.jafarbekyusupov@gmail.com>
2024-10-18 14:00:21 +0500

    Create 977A_Wrong-Subtraction.cpp

---
+++ cpp/977A_Wrong-Subtraction.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, k;
+    cin >> n >> k;
+    for(int i=0; i<k; i++){ n%10 == 0 ? n/=10 : --n; }
+    cout << n;
+    return 0;
+}
